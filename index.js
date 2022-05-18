b71328497a7188d5f15b1165b62daa32fb808a88
Catchthembodies <103537060+Catchthembodies@users.noreply.github.com>
2022-05-17 21:22:54 -0700

    commit tree 2b2a9f353e89c45d9ff29d520a728b5b1d11fa4c

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// to multiply
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+//to find random
+let num6;
+function random () {
+
+    return Math.random(num6)>0;
+  }
+
+
+let num3 = 27;
+let num4 = 6;
+
+const mod = num3 % num4;
